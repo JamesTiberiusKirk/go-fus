cd221fd5f499b6051448571e3c36fab620fe8b6f
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2023-02-23 10:30:03 +0000

    Updates readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## TODO: 
 - [ ] Refactor all of the path manipulation to use filepath.Joins, etc...
+- Build multiple file handlers
+  - [ ] File handler for supporting templates be next to the respective go files
+  - [ ] File handler for supporting multiple template files
+    - I.E. templates folder in components package, pages package, etc
--- renderer/fileHandlers.go
+++ renderer/fileHandlers.go
@@ -12,6 +12,23 @@ import (
 func defaultFileHandler() FileHandler {
 	return func(config Config, tplFile string) (string, error) {
 		// Get the absolute path of the root template
+		path, err := filepath.Abs(config.Root + string(os.PathSeparator) + tplFile)
+		if err != nil {
+			return "", fmt.Errorf("ViewEngine path:%v error: %w", path, err)
+		}
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return "", fmt.Errorf("ViewEngine render read name:%v, path:%v, error: %w", tplFile, path, err)
+		}
+		return string(data), nil
+	}
+}
+
+// InFolderFileHandler new default file handler
+// This is just supposed to read the file and return a string as content.
+func InFolderFileHandler() FileHandler {
+	return func(config Config, tplFile string) (string, error) {
+		// Get the absolute path of the root template
 
 		log.Println("tplFile:", tplFile)
 
